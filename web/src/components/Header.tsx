c23c907b78f90840fbb83fc1e3b29b118362c4f9
Jose Sanchez <engineer@copydataai.com>
2024-05-01 19:49:15 +0100

    feat(components): add header

---
+++ web/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { NavBar } from "@/components/NavBar"
+
+export function Header() {
+    return (
+        <header className="flex justify-center items-center">
+            <NavBar />
+        </header>
+    );
+}
