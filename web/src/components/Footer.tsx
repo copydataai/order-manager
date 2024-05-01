3ab0c0ffe8a8ffac7db8e813f121daeadde17ce9
Jose Sanchez <engineer@copydataai.com>
2024-05-01 20:48:05 +0100

    feat(components): add Footer

---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,36 @@
+import { Logo } from "@/components/Logo"
+import { Icons } from "@/components/icons"
+
+
+export function Footer() {
+    return (
+        <footer className="flex flex-col items-center px-4 gap-4">
+            {/* top */}
+            <div className="flex gap-4 w-1/2 justify-between">
+                <div>
+                    <div className="flex gap-2 items-center">
+                        <Logo />
+                        <h3 className="text-2xl font-bold">Orders</h3>
+                    </div>
+                </div>
+
+                <div className="flex flex-col gap-2">
+                    <h3 className="text-lg font-bold">Legal</h3>
+                    <p className="text-sm">privacy</p>
+                    <p className="text-sm">terms</p>
+                </div>
+            </div>
+
+            {/* bottom */}
+            <div className="flex gap-4 w-1/2 justify-between">
+                <p>Copyright © 2024 Jose Sanchez</p>
+
+                <div className="flex gap-2 items-center">
+                    <Icons.linkedin className="h-10 w-10" />
+                    <Icons.gitHub className="h-8 w-8"/>
+                </div>
+
+            </div>
+        </footer>
+    );
+}
