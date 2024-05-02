6f2f2c4734e870d07df97ab52f151da42fc06586
Jose Sanchez <engineer@copydataai.com>
2024-05-02 08:56:07 +0100

    feat(components): add Hero

---
+++ web/src/components/Hero.tsx
@@ -0,0 +1,20 @@
+
+import { Logo } from "@/components/Logo";
+import { Input } from "@/ui/input";
+import { Button } from "@/ui/button";
+
+
+export function Hero() {
+    return (
+        <section className="flex items-center justify-center w-full h-[calc(80vh-2rem)] gap-2 ">
+            <div className="flex flex-col items-center justify-center gap-2">
+                <Logo className="w-96 h-96" />
+                <p className="text-2xl text-muted-foreground">Make your ideal system</p>
+            </div>
+            <div className="flex w-full max-w-sm items-center space-x-2">
+                <Input type="email" placeholder="Email" />
+                <Button type="submit">Join!</Button>
+            </div>
+        </section>
+    )
+}
