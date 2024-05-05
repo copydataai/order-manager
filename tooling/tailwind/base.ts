58643406b1951a82ac5ea69e132766e0925ae667
Jose Sanchez <engineer@copydataai.com>
2024-05-05 21:12:35 +0100

    fix(tooling): tailwind config

--- tooling/tailwind/base.ts
+++ tooling/tailwind/base.ts
@@ -1,10 +1,8 @@
 import type { Config } from "tailwindcss";
 
-const config = {
+export default {
     darkMode: ["class"],
-    content: [
-        '/src/**/*.{ts,tsx}',
-    ],
+    content: ["src/**/*.{ts,tsx}"],
     theme: {
         extend: {
             colors: {
@@ -42,26 +40,9 @@ const config = {
                     foreground: "hsl(var(--card-foreground))",
                 },
             },
-            borderRadius: {
-                lg: "var(--radius)",
-                md: "calc(var(--radius) - 2px)",
-                sm: "calc(var(--radius) - 4px)",
-            },
-            keyframes: {
-                "accordion-down": {
-                    from: { height: "0" },
-                    to: { height: "var(--radix-accordion-content-height)" },
-                },
-                "accordion-up": {
-                    from: { height: "var(--radix-accordion-content-height)" },
-                    to: { height: "0" },
-                },
-            },
-            animation: {
-                "accordion-down": "accordion-down 0.2s ease-out",
-                "accordion-up": "accordion-up 0.2s ease-out",
+            borderColor: {
+                DEFAULT: "hsl(var(--border))",
             },
         },
     },
-    plugins: [require("tailwindcss-animate")],
 } satisfies Config;
