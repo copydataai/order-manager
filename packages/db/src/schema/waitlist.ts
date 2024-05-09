105c85106db8059dfa845659ba4bbab5a6f86a09
Jose Sanchez <engineer@copydataai.com>
2024-05-09 10:16:16 +0100

    migration(db): add waitlist email unique

--- packages/db/src/schema/waitlist.ts
+++ packages/db/src/schema/waitlist.ts
@@ -2,7 +2,7 @@ import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const waitlist = pgTable("waitlist", {
     id: serial("id").primaryKey(),
-    email: varchar("email", { length: 255 }).notNull(),
+    email: varchar("email", { length: 255 }).unique().notNull(),
     created_at: timestamp("created_at", { mode: "date" })
         .defaultNow()
         .notNull(),
