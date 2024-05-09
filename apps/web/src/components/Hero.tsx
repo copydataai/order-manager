bfc18906d79d3dcce98d3a8a91cc3db129d90f00
Jose Sanchez <engineer@copydataai.com>
2024-05-09 09:57:03 +0100

    feat(web): add WaitlistForm

--- apps/web/src/components/Hero.tsx
+++ apps/web/src/components/Hero.tsx
@@ -2,8 +2,9 @@ import { Button } from "@order/ui/button";
 import { Input } from "@order/ui/input";
 
 import { Logo } from "~/components/Logo";
+import { WaitlistForm } from "~/components/Waitlist";
 
-export function Hero() {
+export async function Hero() {
   return (
     <section className="flex h-[calc(80vh-2rem)] w-full flex-col items-center justify-center gap-2 lg:flex-row ">
       <div className="flex flex-col  items-center justify-center gap-2">
@@ -11,8 +12,7 @@ export function Hero() {
         <p className="text-2xl text-muted-foreground">Make your ideal system</p>
       </div>
       <div className="flex w-full max-w-sm items-center space-x-2">
-        <Input type="email" placeholder="Email" />
-        <Button type="submit">Join!</Button>
+        <WaitlistForm />
       </div>
     </section>
   );
