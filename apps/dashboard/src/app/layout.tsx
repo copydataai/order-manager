c5685bebc23176a67cc60db1d764c51c130dd35d
Jose Sanchez <engineer@copydataai.com>
2024-05-10 23:08:46 +0100

    feat(dashboard): add TRPCReactProvider

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -1,25 +1,25 @@
-import "@/globals.css"
+import "~/app/globals.css";
 
 import type { Metadata } from "next";
 
-
+import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata: Metadata = {
-    title: "Orders' Dashboard",
-    description: "Manage your orders dashboard",
-    icons: [{ rel: "icon", url: "/vite.svg" }],
-}
+  title: "Orders' Dashboard",
+  description: "Manage your orders dashboard",
+  icons: [{ rel: "icon", url: "/vite.svg" }],
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-    return(
-        <html lang="en" >
-            <body>
-                {children}
-            </body>
-        </html>
-    )
+  return (
+    <html lang="en">
+      <body>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
+    </html>
+  );
 }
