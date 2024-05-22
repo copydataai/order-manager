52f13f0795f39a38a8832df64e72ad8ebc1f9af8
Jose Sanchez <engineer@copydataai.com>
2024-05-22 10:49:09 +0100

    feat(api): add organization router

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,8 +1,10 @@
 import { authRouter } from "./router/auth";
+import { organizationRouter } from "./router/organizations";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
-  auth: authRouter,
+    auth: authRouter,
+    organization: organizationRouter,
 });
 
 // export type definition of API
