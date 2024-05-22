eb0dc027a8d80616ee4836f0f02e0e761006b185
Jose Sanchez <engineer@copydataai.com>
2024-05-22 10:51:20 +0100

    feat(dashboard): add DialogOrganization component

---
+++ apps/dashboard/src/components/DialogOrganization.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@order/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@order/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@order/ui/form";
+import { Input } from "@order/ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { api } from "~/trpc/react";
+
+const organizationSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  location: z.string().min(1, { message: "Location is required" }),
+  contactInfo: z.string().min(1, { message: "Contact Info is required" }),
+});
+
+export function DialogOrganization() {
+  const form = useForm<z.infer<typeof organizationSchema>>({
+    resolver: zodResolver(organizationSchema),
+    defaultValues: {
+      name: "",
+      location: "",
+      contactInfo: "",
+    },
+  });
+
+  const mutation = api.organization.create.useMutation({
+    onSuccess: (data) => {
+      console.log("data", data);
+    },
+    onError: (error) => {
+      console.log("error", error);
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+    await mutation.mutate(values);
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Create Organization</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create Organization</DialogTitle>
+          <DialogDescription>
+            Open an organization to start selling.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="The cousine" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Location</FormLabel>
+                  <FormControl>
+                    <Input placeholder="New York" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="contactInfo"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Contact Info</FormLabel>
+                  <FormControl>
+                    <Input placeholder="pasta@email.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
