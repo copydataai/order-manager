249e02f54440c7099098ca75d592742d8d78d8d1
Jose Sanchez <engineer@copydataai.com>
2024-05-23 17:40:37 +0100

    feat(api): add organization route

---
+++ packages/api/src/router/organizations.ts
@@ -0,0 +1,40 @@
+import type { TRPCRouterRecord } from "@trpc/server";
+import { schema } from "@order/db";
+import { z } from "zod";
+
+import { publicProcedure } from "../trpc";
+
+export const organizationRouter = {
+    // TODO: change public procedure to protected exept read by id or by name
+    create: publicProcedure
+        .input(
+            z.object({
+                name: z.string(),
+                location: z.string(),
+                contactInfo: z.string(),
+            }),
+        )
+        .mutation(async ({ input, ctx }) => {
+            const { name, location, contactInfo } = input;
+            const { user } = ctx;
+            if (!user) {
+                console.log("no auth");
+            }
+            console.log(input);
+            const data = await ctx.db.insert(schema.organization).values({
+                name,
+                location,
+                contactInfo,
+            });
+            return data;
+        }),
+    listByName: publicProcedure
+        .input(z.object({ name: z.string() }))
+        .query(({ input, ctx }) => {
+            const { name } = input;
+            return ctx.db.select().from(schema.organization).where({ name });
+        }),
+    listAll: publicProcedure.query(({ ctx }) => {
+        return ctx.db.select().from(schema.organization);
+    }),
+};
