c9af0054bed5259aa693ae3eb14089303cf3dd79
Jose Sanchez <engineer@copydataai.com>
2024-05-24 22:43:01 +0100

    feat(api): add product router

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,9 +1,11 @@
 import { authRouter } from "./router/auth";
 import { organizationRouter } from "./router/organizations";
+import { productRouter } from "./router/products";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
     auth: authRouter,
+    product: productRouter,
     organization: organizationRouter,
 });
 
