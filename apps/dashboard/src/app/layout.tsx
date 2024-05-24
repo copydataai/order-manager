3880000d350aa4d728a1927a931759b6838df0c9
Jose Sanchez <engineer@copydataai.com>
2024-05-24 10:40:55 +0100

    chore(dashboard): add Header&Footer to the layout

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -3,6 +3,8 @@ import "~/app/globals.css";
 import type { Metadata } from "next";
 import { ThemeProvider, ThemeToggle } from "@order/ui/theme";
 
+import { Footer } from "~/components/Footer.tsx";
+import { Header } from "~/components/Header.tsx";
 import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata: Metadata = {
@@ -20,7 +22,11 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <TRPCReactProvider>{children}</TRPCReactProvider>
+          <TRPCReactProvider>
+            <Header />
+            {children}
+            <Footer />
+          </TRPCReactProvider>
           <div className="absolute bottom-4 right-4">
             <ThemeToggle />
           </div>
--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,14 +1,13 @@
-import {Header} from "@/components/Header.tsx"
-import {Footer} from "@/components/Footer.tsx"
+import { Footer } from "~/components/Footer.tsx";
+import { Header } from "~/components/Header.tsx";
+import { Hero } from "~/components/Hero.tsx";
 
 function App() {
-
   return (
     <>
-      <Header />
-      <Footer />
+      <Hero />
     </>
-  )
+  );
 }
 
 export default App;
