efb1bc587a98c455e862e91cfb44b46ad92e18dc
Jose Sanchez <engineer@copydataai.com>
2024-05-24 10:41:52 +0100

    feat(dashboard): add Navbar to header

--- apps/dashboard/src/components/Header.tsx
+++ apps/dashboard/src/components/Header.tsx
@@ -1,7 +1,9 @@
+import { NavBar } from "~/components/Navbar";
+
 export function Header() {
-   return (
-    <header className="flex items-center justify-center p-2">
-        <h1 className="text-4xl">Order-manager</h1>
+  return (
+    <header className="flex items-center justify-center">
+      <NavBar />
     </header>
-    )
+  );
 }
