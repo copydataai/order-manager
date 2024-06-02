79c9281a1ff04a3dc6d456acaa0c4a2c76a3083d
Jose Sanchez <engineer@copydataai.com>
2024-06-02 10:27:51 +0100

    chore(web): use lucide-react for icons and link

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -1,16 +1,21 @@
-import { Icons } from "~/components/icons";
+import Link from "next/link";
+import { Copyright, Github, Linkedin } from "lucide-react";
+
 import { Logo } from "~/components/Logo";
+import { SocialLinks } from "~/config";
 
 export function Footer() {
   return (
-    <footer className="flex w-full flex-col items-center gap-4 px-4 py-4">
+    <footer className="flex w-full flex-col items-center gap-4 px-4 py-6">
       {/* top */}
       <div className="flex w-full justify-between gap-4 px-4 md:w-1/2">
         <div>
-          <div className="flex items-center gap-2">
-            <Logo className="h-16 w-16" />
-            <h3 className="text-2xl font-bold">Orders</h3>
-          </div>
+          <Link href="/">
+            <div className="flex items-center gap-2">
+              <Logo className="h-16 w-16" />
+              <h3 className="text-2xl font-bold">Orders</h3>
+            </div>
+          </Link>
         </div>
 
         <div className="flex flex-col gap-2">
@@ -21,12 +26,20 @@ export function Footer() {
       </div>
 
       {/* bottom */}
-      <div className="flex w-full justify-between gap-4 px-4 py-4 md:w-1/2">
-        <p>Copyright © 2024 Jose Sanchez</p>
+      <div className="flex w-full justify-between gap-4 px-4 md:w-1/2">
+        <p className="inline-flex items-center gap-1 px-4 text-sm">
+          Jose Sanchez
+          <Copyright size="18" />
+          2024
+        </p>
 
         <div className="flex items-center gap-2">
-          <Icons.linkedin className="h-10 w-10" />
-          <Icons.gitHub className="h-8 w-8" />
+          <Link href={SocialLinks.Linkedin}>
+            <Linkedin />
+          </Link>
+          <Link href={SocialLinks.Github}>
+            <Github />
+          </Link>
         </div>
       </div>
     </footer>
