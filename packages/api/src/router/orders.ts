1ddca576947df70ba29c2376abf0ff4fa43b3789
Jose Sanchez <engineer@copydataai.com>
2024-06-11 17:27:00 +0100

    chore(api): add order endpoints

--- packages/api/src/router/orders.ts
+++ packages/api/src/router/orders.ts
@@ -37,6 +37,66 @@ export const orderRouter = {
 
             return data;
         }),
+
+    createOrderDetail: publicProcedure
+        .input(
+            z.object({
+                productId: z.number(),
+                orderId: z.number(),
+                quantity: z.number(),
+                status: z.string(),
+                lineTotal: z.number(),
+            }),
+        )
+        .mutation(async ({ input, ctx }) => {
+            const { productId, orderId, status, quantity, lineTotal } = input;
+
+            const data = await ctx.db.insert(schema.orderdetails).values({
+                productId,
+                orderId,
+                status,
+                quantity,
+                lineTotal,
+            });
+
+            return data;
+        }),
+
+    createOrderAndOrderDetails: publicProcedure
+        .input(
+            z.object({
+                productId: z.number(),
+                orderId: z.number(),
+                quantity: z.number(),
+                lineTotal: z.number(),
+                orderDetails: z.array(
+                    z.object({
+                        orderDate: z.date(),
+                        customerName: z.string(),
+                        // TODO add enum statusorder by db.schema
+                        status: z.string(),
+                        totalAmount: z.number(),
+                        organizationId: z.number(),
+                    }),
+                ),
+            }),
+        )
+        .mutation(async ({ input, ctx }) => {
+            console.log(input);
+            return "thank you";
+        }),
+
+    listDetailsByOrderId: publicProcedure
+        .input(z.object({ orderId: z.number() }))
+        .query(async ({ input, ctx }) => {
+            const { orderId } = input;
+            console.log(orderId);
+            const orderDetails = await ctx.db
+                .select()
+                .from(schema.orderdetails)
+                .where(eq(schema.orderdetails.orderId, orderId));
+            return orderDetails;
+        }),
     listAllByOrganizationId: publicProcedure //
         .input(z.object({ organizationId: z.number() }))
         .query(async ({ input, ctx }) => {
@@ -49,4 +109,9 @@ export const orderRouter = {
 
             return orders;
         }),
+    // TODO: add input by user and check which organization it belongs
+    listAll: publicProcedure.query(async ({ ctx }) => {
+        const orders = await ctx.db.select().from(schema.order);
+        return orders;
+    }),
 };
