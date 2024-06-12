f25991893d8c4fdf8ff496f977a792b1fe9f99df
Jose Sanchez <engineer@copydataai.com>
2024-06-12 10:50:23 +0100

    feat(dashboard): add data-table for orders by org

--- apps/dashboard/src/app/organization/[id]/orders/page.tsx
+++ apps/dashboard/src/app/organization/[id]/orders/page.tsx
@@ -1,22 +1,97 @@
-import { DialogOrder } from "~/components/DialogOrders";
-import { OrderList } from "~/components/OrdersList";
-import { api } from "~/trpc/server";
+"use client";
 
-export default async function OrdersPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+import { Button } from "@order/ui/button";
+import { DataTable } from "@order/ui/data-table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@order/ui/dropdown-menu";
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+
+import { DialogOrder } from "~/components/DialogOrders";
+import { api } from "~/trpc/react";
+
+type Order = {
+  orderId: number;
+  orderDate: Date;
+  customerName: string;
+  status: string;
+  totalAmount: number;
+};
+
+export const columns: ColumnDef<Order>[] = [
+  {
+    accessorKey: "order.orderId",
+    header: "Order ID",
+  },
+  {
+    accessorKey: "order.orderDate",
+    header: "Order Date",
+  },
+  {
+    accessorKey: "order.customerName",
+    header: "Customer Name",
+  },
+  {
+    accessorKey: "order.status",
+    header: "Status",
+  },
+  {
+    accessorKey: "order.totalAmount",
+    header: "Total Amount",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const organization = row.original.organization;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            {/* TODO: add dialog or modal to edit specific order */}
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(organization.name)}
+            >
+              edit
+            </DropdownMenuItem>
+            <DropdownMenuItem>show details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
+export default function OrdersPage({ params }: { params: { id: string } }) {
   const { id } = params;
   // parsing from string to number, because nextjs just can read params as a string
   const organizationId = Number(id);
 
-  const orders = await api.order.listAllByOrganizationId({ organizationId });
+  const { data, isLoading } = api.order.listAllByOrganizationId.useQuery({
+    organizationId,
+  });
+
+  if (isLoading)
+    return (
+      <section className="flex flex-col items-center justify-center gap-4 px-4 py-16">
+        <div>Loading...</div>
+      </section>
+    );
 
   return (
-    <section className="flex flex-col items-center justify-center gap-12 px-4 py-16">
+    <section className="flex flex-col items-center justify-center gap-4 px-4 py-16">
       <DialogOrder organizationId={id} />
-      <OrderList orders={orders} />
+      <DataTable data={data} columns={columns} />
     </section>
   );
 }
