d3d053fa88d6ca724b797c7f1c3b1ceb12db6747
Jose Sanchez <engineer@copydataai.com>
2024-06-12 22:39:33 +0100

    fix(api): throw error in auth by error auth

--- packages/api/src/router/auth.ts
+++ packages/api/src/router/auth.ts
@@ -1,4 +1,5 @@
 import type { TRPCRouterRecord } from "@trpc/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { protectedProcedure, publicProcedure } from "../trpc";
@@ -7,12 +8,21 @@ export const authRouter = {
     signup: publicProcedure
         .input(z.object({ email: z.string(), password: z.string() }))
         .mutation(async ({ ctx, input }) => {
-            const res = await ctx.auth.signUp({
+            const { data, error } = await ctx.auth.signUp({
                 email: input.email,
                 password: input.password,
             });
+            if (error) {
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: error.message,
+                });
+            }
 
-            return res;
+            ctx.user = data.user;
+            ctx.auth = data.session;
+
+            return data;
         }),
     login: publicProcedure
         .input(z.object({ email: z.string(), password: z.string() }))
@@ -21,7 +31,14 @@ export const authRouter = {
                 email: input.email,
                 password: input.password,
             });
-            // ctx.session = data.session;
+
+            if (error) {
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: error.message,
+                });
+            }
+
             ctx.user = data.user;
             ctx.auth = data.session;
 
