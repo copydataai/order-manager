c69fa6085087cfd58094025233346d1226cdae3b
Jose Sanchez <engineer@copydataai.com>
2024-06-14 21:53:03 +0100

    fix(OrganizationList): rending by react query

--- apps/dashboard/src/components/OrganizationList.tsx
+++ apps/dashboard/src/components/OrganizationList.tsx
@@ -1,22 +1,40 @@
+"use client";
+
 import Link from "next/link";
 
+import { redirectAuth } from "~/actions/redirect";
 import { OrganizationCard } from "~/components/OrganizationCard";
+import { api } from "~/trpc/react";
 
-export function OrganizationList(props: { organizations: any[] }) {
-  console.log(props.organizations.length);
-  if (props.organizations.length === 0) {
+export function OrganizationList() {
+  const {
+    data = [],
+    isLoading,
+    isError,
+    error,
+  } = api.organization.listAll.useQuery();
+
+  if (isLoading) {
     return (
       <div className="flex w-full flex-col gap-4">
         <div className="flex flex-col items-center justify-center ">
-          <p className="text-2xl font-bold ">No organizations yet</p>
+          <p className="text-2xl font-bold ">Loading ... </p>
         </div>
       </div>
     );
   }
 
+  if (isError) {
+    console.log("hell", isError, error.shape.message);
+    if (error.shape.message === "UNAUTHORIZED") {
+      console.log("hell", isError);
+      redirectAuth();
+    }
+  }
+
   return (
     <div className="grid w-full auto-cols-auto grid-flow-col gap-4 ">
-      {props.organizations.map((org) => {
+      {data.map((org) => {
         return (
           <Link
             href="/organization/[id]"
