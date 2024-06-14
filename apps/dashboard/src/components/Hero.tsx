001f8cf1b20984ace6ba96d404d2018c8d83377a
Jose Sanchez <engineer@copydataai.com>
2024-06-14 21:39:22 +0100

    feat(dashboard): add DialogRole

--- apps/dashboard/src/components/Hero.tsx
+++ apps/dashboard/src/components/Hero.tsx
@@ -1,18 +1,19 @@
 import { Button } from "@order/ui/button";
 
 import { DialogOrganization } from "~/components/DialogOrganization";
+import { DialogRole } from "~/components/DialogRole";
 import { OrganizationList } from "~/components/OrganizationList";
 import { api } from "~/trpc/server";
 
 export async function Hero() {
-  const organizations = await api.organization.listAll();
   return (
     <section className="grip-2 flex justify-center space-x-2">
       <h2 className="text-3xl font-black">Organizations</h2>
       <DialogOrganization className="" />
+      <DialogRole />
       <div className="flex w-1/2 flex-col items-center space-y-4 bg-[#F5F5F5]">
         <div className="">
-          <OrganizationList organizations={organizations} />
+          <OrganizationList />
         </div>
         <div></div>
       </div>
