2d9891c5fe4264abee941b6fa9d27172a1889dd3
Jose Sanchez <engineer@copydataai.com>
2024-06-19 22:53:33 +0100

    feat(dashboard): add PHProvider on dashboards

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -1,11 +1,17 @@
 import "~/app/globals.css";
 
 import type { Metadata } from "next";
+import dynamic from "next/dynamic";
 import { ThemeProvider, ThemeToggle } from "@order/ui/theme";
 
+import { PHProvider } from "~/app/provider";
 import { Footer } from "~/components/Footer.tsx";
 import { Header } from "~/components/Header.tsx";
 import { TRPCReactProvider } from "~/trpc/react";
+
+const PostHogPageView = dynamic(() => import("./PostHogPageView"), {
+  ssr: false,
+});
 
 export const metadata: Metadata = {
   title: "Orders' Dashboard",
@@ -20,18 +26,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <TRPCReactProvider>
-            <Header />
-            {children}
-            <Footer />
-          </TRPCReactProvider>
-          <div className="absolute bottom-4 right-4">
-            <ThemeToggle />
-          </div>
-        </ThemeProvider>
-      </body>
+      <PHProvider>
+        <body>
+          <PostHogPageView />
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <TRPCReactProvider>
+              <Header />
+              {children}
+              <Footer />
+            </TRPCReactProvider>
+            <div className="absolute bottom-4 right-4">
+              <ThemeToggle />
+            </div>
+          </ThemeProvider>
+        </body>
+      </PHProvider>
     </html>
   );
 }
