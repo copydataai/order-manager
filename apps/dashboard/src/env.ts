124439d54b8b2f2b3d3d18f9a6a46fd56acfd10f
Jose Sanchez <engineer@copydataai.com>
2024-06-20 08:01:41 +0100

    fix(dashboard): add runtimeEnv and validation

--- apps/dashboard/src/env.ts
+++ apps/dashboard/src/env.ts
@@ -27,8 +27,8 @@ export const env = createEnv({
      * For them to be exposed to the client, prefix them with `NEXT_PUBLIC_`.
      */
     client: {
-        NEXT_PUBLIC_POSTHOG_KEY: z.string(),
-        NEXT_PUBLIC_POSTHOG_HOST: z.string(),
+        NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1),
+        NEXT_PUBLIC_POSTHOG_HOST: z.string().url(),
     },
     /**
      * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
@@ -36,7 +36,8 @@ export const env = createEnv({
     experimental__runtimeEnv: {
         NODE_ENV: process.env.NODE_ENV,
 
-        // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+        NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+        NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     },
     skipValidation:
         !!process.env.CI ||
