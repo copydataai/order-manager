265b4803521b0e267acd1b2520b0f8e926a67d11
Jose Sanchez <engineer@copydataai.com>
2024-06-22 17:25:12 +0100

    chore(web): add SiteLinks config

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -2,3 +2,7 @@ export const SocialLinks = {
     Github: "https://github.com/copydataai",
     Linkedin: "https://www.linkedin.com/in/copydataai",
 };
+
+export const SiteLinks = {
+    Dashboard: "https://dashboard.copydataai.com",
+};
