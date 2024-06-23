11a0ec8af8614de906abd20d2b95e05a5c2017d0
Jose Sanchez <engineer@copydataai.com>
2024-06-23 10:37:36 +0100

    feat(api): add schemasZod

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,6 +3,7 @@ import postgres from "postgres";
 
 import { connectionStr } from "./config";
 import * as schemas from "./schema/schema";
+import * as schemasZod from "./schema/schemasZod";
 import * as waitlist from "./schema/waitlist";
 
 export * from "drizzle-orm/sql";
@@ -10,6 +11,10 @@ export * from "drizzle-orm/sql";
 export { drizzle } from "drizzle-orm/postgres-js";
 
 export const schema = { ...waitlist, ...schemas };
+
+export const schemaZod = {
+    ...schemasZod,
+};
 
 const psClient = postgres(connectionStr.href, { prepare: false });
 export const db = drizzle(psClient, { schema });
---
+++ packages/db/src/schema/schemasZod.ts
@@ -0,0 +1,39 @@
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+
+import { order, orderdetails, organization, product } from "./schema";
+
+export const statusOrderSchema = z.enum(["FINISHED", "CANCELLED", "ORDERED"]);
+
+export const organizationSchema = createSelectSchema(organization);
+
+export const createOrderSchema = createInsertSchema(order, {
+    orderDate: z.date(),
+    customerName: z.string(),
+    status: statusOrderSchema,
+    totalAmount: z.number(),
+    organizationId: z.number(),
+}).omit({
+    orderId: true,
+    //TODO: add created_at, updated_at
+});
+
+export const createOrderDetailSchema = createInsertSchema(orderdetails, {
+    orderId: z.number(),
+    productId: z.number(),
+    quantity: z.number(),
+    lineTotal: z.number(),
+}).omit({
+    orderDetailId: true,
+    //TODO: add created_at, updated_at
+});
+
+export const createProductSchema = createInsertSchema(product, {
+    name: z.string(),
+    description: z.string(),
+    price: z.number().or(z.string()).pipe(z.coerce.number()),
+    organizationId: z.number().or(z.string()).pipe(z.coerce.number()),
+}).omit({
+    productId: true,
+    //TODO: add created_at, updated_at
+});
