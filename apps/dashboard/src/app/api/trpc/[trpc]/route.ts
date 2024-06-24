ebe4cc00b08dd4754efcc05d94045c745cfedb1d
Jose Sanchez <engineer@copydataai.com>
2024-06-24 17:45:28 +0100

    chore(dashboard): update trpc routes

--- apps/dashboard/src/app/api/trpc/[trpc]/route.ts
+++ apps/dashboard/src/app/api/trpc/[trpc]/route.ts
@@ -1,41 +1,41 @@
+import type { NextRequest } from "next/server";
 import { appRouter, createTRPCContext } from "@order/api";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+
+import { createClient } from "~/utils/supabase/server";
 
 /**
  * Configure basic CORS headers
  * You should extend this to match your needs
  */
-const setCorsHeaders = (res: Response) => {
+function setCorsHeaders(res: Response) {
     res.headers.set("Access-Control-Allow-Origin", "*");
     res.headers.set("Access-Control-Request-Method", "*");
     res.headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
     res.headers.set("Access-Control-Allow-Headers", "*");
-};
-
-export const OPTIONS = () => {
+}
+export function OPTIONS() {
     const response = new Response(null, {
         status: 204,
     });
     setCorsHeaders(response);
     return response;
-};
+}
 
-const handler = async (req) => {
+const handler = async (req: NextRequest) => {
+    const supabase = createClient();
+
     const response = await fetchRequestHandler({
         endpoint: "/api/trpc",
         router: appRouter,
         req,
         createContext: () =>
-            createTRPCContext({
-                headers: req.headers,
-            }),
+            createTRPCContext({ headers: req.headers, supabase }),
         onError({ error, path }) {
             console.error(`>>> tRPC Error on '${path}'`, error);
         },
     });
-
     setCorsHeaders(response);
     return response;
 };
-
 export { handler as GET, handler as POST };
