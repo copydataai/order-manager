0d4528ad8c08fcf4824c642c881caeabfddec044
Jose Sanchez <engineer@copydataai.com>
2024-06-24 17:44:30 +0100

    chore(dashboard): update middleware

--- apps/dashboard/src/middleware.ts
+++ apps/dashboard/src/middleware.ts
@@ -1,22 +1,12 @@
 import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
-    // TODO: change "session" cookie name by other more secure
-    const cookie = request.cookies.get("session");
-    if (cookie) {
-        // TODO: add refresh session supabase jwt
-        return NextResponse.next();
-    }
+import { updateSession } from "~/utils/supabase/middleware";
 
-    if (request.nextUrl.pathname.startsWith("/auth")) {
-        return NextResponse.next();
-    }
-
-    return NextResponse.redirect(new URL("/auth", request.url));
+export async function middleware(request: NextRequest) {
+    return await updateSession(request);
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*.webp|.ico).*)"],
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
