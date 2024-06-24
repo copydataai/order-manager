a9c3df3172902ca2cc96ff395c293a4f4135ca78
Jose Sanchez <engineer@copydataai.com>
2024-06-24 17:46:36 +0100

    chore(dashboard): add supabase envs

--- apps/dashboard/src/env.ts
+++ apps/dashboard/src/env.ts
@@ -29,6 +29,8 @@ export const env = createEnv({
     client: {
         NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1),
         NEXT_PUBLIC_POSTHOG_HOST: z.string().url(),
+        NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
     },
     /**
      * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
@@ -38,6 +40,9 @@ export const env = createEnv({
 
         NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
         NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY:
+            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     },
     skipValidation:
         !!process.env.CI ||
