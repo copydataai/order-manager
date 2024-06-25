59ff3ecd7fcaea778b8285776e6dff1ed9a1010e
Jose Sanchez <engineer@copydataai.com>
2024-06-25 09:37:00 +0100

    fix(dashboard): update Signup with safe-actions

--- apps/dashboard/src/components/Signup.tsx
+++ apps/dashboard/src/components/Signup.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
+import type { SignUp } from "@order/validators";
 import { Button } from "@order/ui/button";
 import {
   Form,
@@ -12,47 +12,28 @@ import {
   FormMessage,
 } from "@order/ui/form";
 import { Input } from "@order/ui/input";
+import { SignUpSchema } from "@order/validators";
+import { useAction } from "next-safe-action/hooks";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { redirectAuth } from "~/actions/redirect";
-import { api } from "~/trpc/react";
-
-const formSchema = z.object({
-  email: z.string().email({ message: "Invalid email address" }),
-  password: z.string().min(1, { message: "Please enter a password" }),
-
-  firstName: z.string().min(1, { message: "Please enter your first name" }),
-  lastName: z.string().min(1, { message: "Please enter your last name" }),
-});
+import { signUp } from "~/app/auth/actions";
 
 export function Signup() {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm({
+    schema: SignUpSchema,
     defaultValues: {
       email: "",
       password: "",
     },
   });
 
-  const message = api.auth.signup.useMutation({
-    onSuccess: async (data) => {
-      console.log("It's okay");
-      console.log(data);
-      await redirectAuth();
-    },
-    onError: (error) => {
-      console.error("It's heaven");
-      console.error(error);
-    },
-  });
-  // 2. Define a submit handler.
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    await message.mutate(values);
-  }
+  const { execute, result, isExecuting } = useAction(signUp);
+
+  const onSubmit = (values: SignUp) => {
+    execute(values);
+  };
 
   return (
     <Form {...form}>
