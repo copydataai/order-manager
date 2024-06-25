088c635faee1cdc4bba00458886169f5615845b3
Jose Sanchez <engineer@copydataai.com>
2024-06-25 10:49:18 +0100

    fix(api): remove authRouter

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,11 +1,9 @@
-import { authRouter } from "./router/auth";
 import { orderRouter } from "./router/orders";
 import { organizationRouter } from "./router/organizations";
 import { productRouter } from "./router/products";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
-    auth: authRouter,
     product: productRouter,
     organization: organizationRouter,
     order: orderRouter,
