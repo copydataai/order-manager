6549d1cb845c9b09ed6ccca94448783df81f624b
Jose Sanchez <engineer@copydataai.com>
2024-06-28 19:19:06 +0100

    chore(dashboard): disable posthog in "development"

--- apps/dashboard/src/app/provider.tsx
+++ apps/dashboard/src/app/provider.tsx
@@ -6,7 +6,7 @@ import { PostHogProvider } from "posthog-js/react";
 
 import { env } from "~/env";
 
-if (typeof window !== "undefined") {
+if (typeof window !== "undefined" && env.NODE_ENV === "development") {
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
