43251566a4088b395da6b111ba4940bc300a51cf
Jose Sanchez <engineer@copydataai.com>
2024-07-08 11:18:40 +0100

    chore(db): delete supabase pgEnum

--- packages/db/src/schema/schema.ts
+++ packages/db/src/schema/schema.ts
@@ -9,73 +9,16 @@ import {
 	primaryKey,
 	serial,
 	timestamp,
-	unique,
 	uuid,
 	varchar,
 } from "drizzle-orm/pg-core";
 
-export const requestStatus = pgEnum("request_status", [
-	"PENDING",
-	"SUCCESS",
-	"ERROR",
-]);
-export const keyStatus = pgEnum("key_status", [
-	"default",
-	"valid",
-	"invalid",
-	"expired",
-]);
-export const keyType = pgEnum("key_type", [
-	"aead-ietf",
-	"aead-det",
-	"hmacsha512",
-	"hmacsha256",
-	"auth",
-	"shorthash",
-	"generichash",
-	"kdf",
-	"secretbox",
-	"secretstream",
-	"stream_xchacha20",
-]);
-export const factorType = pgEnum("factor_type", ["totp", "webauthn"]);
-export const factorStatus = pgEnum("factor_status", ["unverified", "verified"]);
-export const aalLevel = pgEnum("aal_level", ["aal1", "aal2", "aal3"]);
-export const codeChallengeMethod = pgEnum("code_challenge_method", [
-	"s256",
-	"plain",
-]);
 export const typeorder = pgEnum("typeorder", ["EAT_IN", "TAKEAWAY"]);
 export const statusorder = pgEnum("statusorder", [
 	"FINISHED",
 	"CANCELLED",
 	"ORDERED",
 ]);
-export const equalityOp = pgEnum("equality_op", [
-	"eq",
-	"neq",
-	"lt",
-	"lte",
-	"gt",
-	"gte",
-	"in",
-]);
-export const action = pgEnum("action", [
-	"INSERT",
-	"UPDATE",
-	"DELETE",
-	"TRUNCATE",
-	"ERROR",
-]);
-export const oneTimeTokenType = pgEnum("one_time_token_type", [
-	"confirmation_token",
-	"reauthentication_token",
-	"recovery_token",
-	"email_change_token_new",
-	"email_change_token_current",
-	"phone_change_token",
-]);
-
 export const roles = pgTable(
 	"roles",
 	{
