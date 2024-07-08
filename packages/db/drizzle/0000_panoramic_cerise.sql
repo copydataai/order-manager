740ac8a2c99ca58d8aec36636582bda443025db9
Jose Sanchez <engineer@copydataai.com>
2024-07-08 11:17:57 +0100

    fix(db): update sql migration

--- packages/db/drizzle/0000_panoramic_cerise.sql
+++ packages/db/drizzle/0000_panoramic_cerise.sql
@@ -1,77 +1,6 @@
 -- Current sql file was generated after introspecting the database
 -- If you want to run this migration please uncomment this code before executing migrations
-/*
-DO $$ BEGIN
- CREATE TYPE "auth"."aal_level" AS ENUM('aal1', 'aal2', 'aal3');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- CREATE TYPE "auth"."code_challenge_method" AS ENUM('s256', 'plain');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- CREATE TYPE "auth"."factor_status" AS ENUM('unverified', 'verified');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- CREATE TYPE "auth"."factor_type" AS ENUM('totp', 'webauthn');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- CREATE TYPE "auth"."one_time_token_type" AS ENUM('confirmation_token', 'reauthentication_token', 'recovery_token', 'email_change_token_new', 'email_change_token_current', 'phone_change_token');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- CREATE TYPE "net"."request_status" AS ENUM('PENDING', 'SUCCESS', 'ERROR');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- CREATE TYPE "pgsodium"."key_status" AS ENUM('default', 'valid', 'invalid', 'expired');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- CREATE TYPE "pgsodium"."key_type" AS ENUM('aead-ietf', 'aead-det', 'hmacsha512', 'hmacsha256', 'auth', 'shorthash', 'generichash', 'kdf', 'secretbox', 'secretstream', 'stream_xchacha20');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- CREATE TYPE "public"."statusorder" AS ENUM('FINISHED', 'CANCELLED', 'ORDERED');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- CREATE TYPE "public"."typeorder" AS ENUM('EAT_IN', 'TAKEAWAY');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- CREATE TYPE "realtime"."action" AS ENUM('INSERT', 'UPDATE', 'DELETE', 'TRUNCATE', 'ERROR');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- CREATE TYPE "realtime"."equality_op" AS ENUM('eq', 'neq', 'lt', 'lte', 'gt', 'gte', 'in');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
+
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "roles" (
 	"role_id" serial PRIMARY KEY NOT NULL,
@@ -84,23 +13,12 @@ CREATE TABLE IF NOT EXISTS "user" (
 	"last_name" varchar
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "waitlist" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"email" varchar(255) NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "waitlist_email_unique" UNIQUE("email")
-);
---> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "orderdetails" (
 	"order_detail_id" serial PRIMARY KEY NOT NULL,
 	"order_id" integer NOT NULL,
 	"product_id" integer NOT NULL,
 	"quantity" integer NOT NULL,
 	"line_total" numeric NOT NULL
-);
---> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "alembic_version" (
-	"version_num" varchar(32) PRIMARY KEY NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "order" (
@@ -159,19 +77,19 @@ EXCEPTION
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "organization_users" ADD CONSTRAINT "organization_users_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "public"."organization"("organization_id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "organization_users" ADD CONSTRAINT "organization_users_organization_id_organization_organization_id" FOREIGN KEY ("organization_id") REFERENCES "public"."organization"("organization_id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "organization_users" ADD CONSTRAINT "organization_users_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "public"."roles"("role_id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "organization_users" ADD CONSTRAINT "organization_users_role_id_roles_role_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."roles"("role_id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "organization_users" ADD CONSTRAINT "organization_users_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "organization_users" ADD CONSTRAINT "organization_users_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
@@ -185,4 +103,3 @@ CREATE INDEX IF NOT EXISTS "ix_order_status" ON "order" USING btree ("status" en
 CREATE INDEX IF NOT EXISTS "ix_order_total_amount" ON "order" USING btree ("total_amount" numeric_ops);--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "ix_organization_name" ON "organization" USING btree ("name" text_ops);--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "ix_organization_organization_id" ON "organization" USING btree ("organization_id" int4_ops);
-*/
