125d15c2bca3ad89c39ddcc32165cb37acf8e69c
Jose Sanchez <engineer@copydataai.com>
2024-07-16 18:36:47 +0100

    fix(dashboard): fix responsive design hero

--- apps/dashboard/src/components/Hero.tsx
+++ apps/dashboard/src/components/Hero.tsx
@@ -7,16 +7,13 @@ import { api } from "~/trpc/server";
 
 export async function Hero() {
   return (
-    <section className="flex flex-col justify-center space-x-2 space-y-2">
-      <div className="flex items-center justify-center space-x-2 space-y-4">
+    <section className="flex flex-col items-center justify-center space-x-2 space-y-2">
+      <div className="flex flex-col items-center justify-center space-x-2 space-y-4">
         <h2 className="text-3xl font-black tracking-tight">Organizations</h2>
         <DialogOrganization className="" />
       </div>
-      <div className="flex flex-col items-center justify-center space-y-4 bg-accent">
-        <div>
-          <OrganizationList />
-        </div>
-        <div></div>
+      <div className="w-2/3 ">
+        <OrganizationList />
       </div>
     </section>
   );
